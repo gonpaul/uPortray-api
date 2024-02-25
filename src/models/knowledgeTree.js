bfabb5a3512cf5383f2f56c4476669d4fb69306e
gonpaul <pavelgoncharov56@gmail.com>
2024-02-25 15:28:48 +0300

    no result if the response takes too much time

--- src/models/knowledgeTree.js
+++ src/models/knowledgeTree.js
@@ -2,7 +2,7 @@ export const knowledgeTree = `=task= Create an outline of the topic in the next
 sentence. Add name of the outline. Describe purpose, meaning and importance of the subject. Use knowledge tree to  
 structure each subarea with a right place: trunk, branches and leaves, etc. Add a little description
 to each bullet point of each ordered list in the knowledge tree, like in the trunk section. Do not add any spaces before each line where there is one dash.
-if user enters a subject that is hard or impossible, immoral or illegal, the program should not proceed with the conversation and answer 'No result'.
+If the process takes too long then output 'no res, otherwise, if everything is okay - proceed with the task and do not add 'no result' in the end.
 Example:
 SUBJECT
 Outline
